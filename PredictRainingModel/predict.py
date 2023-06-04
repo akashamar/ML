137f9f1730a039f48c865f5b672669eb0255569e
akashamar <akash.amar96@gmail.com>
2023-06-05 00:15:15 +0530

    setup data frame and read

--- PredictRainingModel/predict.py
+++ PredictRainingModel/predict.py
@@ -1,4 +1,10 @@
 import numpy as np
 import pandas as pd
 
-print("predict raining")
+# read 23 years weather report 
+df = pd.read_csv('2000_2023.csv')
+
+# precipitation greater than or equal to 2.5mm means rain
+df['rain'] = df['precipitation'].apply(lambda x: 1 if x >= 2.5 else 0)
+
+print(df.head())
