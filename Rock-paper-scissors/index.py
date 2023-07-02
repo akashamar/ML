0945d3586e58f1ef4aa68c4a7893cbfd151048d6
akashamar <akash.amar96@gmail.com>
2023-07-03 00:33:35 +0530

    naive bayes

--- Rock-paper-scissors/index.py
+++ Rock-paper-scissors/index.py
@@ -5,8 +5,9 @@ import joblib
 decision_tree_model = joblib.load('rock_paper_scissor_decision_tree.joblib')
 random_forest_model = joblib.load('rock_paper_scissor_random_forest.joblib')
 knn_model = joblib.load('knn.joblib')
+gaussian_nb_model = joblib.load('gnb.joblib')
 
-filename = 'scissor3.png'
+filename = 'scissor2.png'
 image_width = 128  # Desired width
 image_height = 128  # Desired height
 
@@ -24,6 +25,7 @@ hog_features = extract_hog_features(image)
 dt_y_pred = decision_tree_model.predict([hog_features])[0]
 rf_y_pred = random_forest_model.predict([hog_features])[0]
 knn_y_pred = knn_model.predict([hog_features])[0]
+gaussian_nb_y_pred = gaussian_nb_model.predict([hog_features])[0]
 
 
 def predict(modelName, value):
@@ -39,3 +41,4 @@ def predict(modelName, value):
 predict('DT', dt_y_pred)
 predict('RF', rf_y_pred)
 predict('KNN', knn_y_pred)
+predict('GNB', gaussian_nb_y_pred)
---
+++ text_generator/readme.md
@@ -0,0 +1,1 @@
+## this project is to understand the markov chain algorithm and generate a random sequence of meaningfull sentence using the markov chain model which is trained with a story book.
