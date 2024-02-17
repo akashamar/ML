93fbdbefb5b511a7d4337368bd0aa146db64544e
akashamar <akash.amar96@gmail.com>
2024-02-17 13:45:34 +0530

    detectNumberplate.py

--- Neural_network/Yolo/detectNumberplate.py
+++ Neural_network/Yolo/detectNumberplate.py
@@ -1,13 +1,29 @@
-from inference_sdk import InferenceHTTPClient
+from roboflow import Roboflow
+import supervision as sv
 from dotenv import load_dotenv
 import os
+import cv2
 
 load_dotenv()
 
-CLIENT = InferenceHTTPClient(
-    api_url="https://detect.roboflow.com",
-    api_key=os.getenv('ROBO_API_KEY')
-)
+rf = Roboflow(api_key=os.getenv('ROBO_API_KEY'))
+project = rf.workspace().project("carplate-xuk6s")
+model = project.version(1).model
 
-result = CLIENT.infer("https://media.geeksforgeeks.org/wp-content/uploads/20200326001003/blurred.jpg", model_id="carplate-xuk6s/1")
-print(result)
+result = model.predict("blurred.jpg", confidence=40, overlap=30).json()
+
+labels = [item["class"] for item in result["predictions"]]
+
+detections = sv.Detections.from_inference(result)
+
+label_annotator = sv.LabelAnnotator()
+bounding_box_annotator = sv.BoxAnnotator()
+
+image = cv2.imread("blurred.jpg")
+
+annotated_image = label_annotator.annotate(
+    scene=image, detections=detections)
+annotated_image = label_annotator.annotate(
+    scene=annotated_image, detections=detections, labels=labels)
+
+sv.plot_image(image=annotated_image, size=(16, 16))
